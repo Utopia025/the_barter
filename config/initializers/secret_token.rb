a090df081874a262caef4329d887c64de19034e5
Utopia025 <scarr025@gmail.com>
2014-01-09 16:46:37 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheBarter::Application.config.secret_key_base = '62d29fda764750a018f1f36118d4f4ac2ea23444d81b59eeaad403e62666a5cdeb9f2180028af15937ff177c49ffbe3f3d81f78d5ba30a049fa264c81725efd8'
